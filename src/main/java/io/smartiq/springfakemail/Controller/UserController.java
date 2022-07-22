4b40405e3c1c4a3a9005f009d03489ef1ed0951c
Yusuf Yildirim <yusufyildirim1025@gmail.com>
2022-07-23 00:31:22 +0300

    created role classes including controller classes. tested in postman

--- src/main/java/io/smartiq/springfakemail/Controller/UserController.java
+++ src/main/java/io/smartiq/springfakemail/Controller/UserController.java
@@ -1,7 +1,10 @@
 package io.smartiq.springfakemail.Controller;
 
+import io.smartiq.springfakemail.DTO.RoleDTO;
 import io.smartiq.springfakemail.DTO.UserDTO;
 import io.smartiq.springfakemail.Service.IUserService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -9,16 +12,11 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/user")
 public class UserController {
     @Autowired
     private IUserService userService;
-    /*
-    @ResponseStatus(HttpStatus.OK)
-    @GetMapping("{userId}")
-    public UserDTO getUser(@PathVariable("{userId}") Long userId){
-    }
-     */
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public UserDTO saveUser(@RequestBody UserDTO userDTO){
@@ -39,4 +37,20 @@ public class UserController {
     public void delete(@PathVariable("id") Long id){
         userService.delete(id);
     }
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/role")
+    public RoleDTO saveRole(@RequestBody RoleDTO roleDTO){
+        System.out.println(roleDTO);
+        return userService.saveRole(roleDTO);
+    }
+    @ResponseStatus(HttpStatus.OK)
+    @PostMapping("/role/addtouser")
+    public void addRoleToUser(@RequestBody RoleToUserForm form){
+        userService.addRoleToUser(form.getUsername(), form.getRoleName());
+    }
+}
+@Data
+class RoleToUserForm{
+    private String username;
+    private String roleName;
 }
--- src/main/java/io/smartiq/springfakemail/DTO/RoleDTO.java
+++ src/main/java/io/smartiq/springfakemail/DTO/RoleDTO.java
@@ -1,4 +1,11 @@
 package io.smartiq.springfakemail.DTO;
 
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
 public class RoleDTO {
+    private Long id;
+    private String name;
 }
