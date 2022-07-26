57b9cd159d85c0c5b150b65998a85ce9452cf951
Yusuf Yildirim <yusufyildirim1025@gmail.com>
2022-07-26 19:52:15 +0300

    added swagger documentation for api

--- src/main/java/io/smartiq/springfakemail/SpringFakeMailApplication.java
+++ src/main/java/io/smartiq/springfakemail/SpringFakeMailApplication.java
@@ -3,19 +3,26 @@ package io.smartiq.springfakemail;
 import io.smartiq.springfakemail.DTO.RoleDTO;
 import io.smartiq.springfakemail.DTO.UserDTO;
 import io.smartiq.springfakemail.Service.IUserService;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class SpringFakeMailApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringFakeMailApplication.class, args);
     }
+    //localhost:8080/swagger-ui address is host of documentation.
 
     @Bean
     PasswordEncoder passwordEncoder(){
@@ -42,5 +49,15 @@ public class SpringFakeMailApplication {
             userService.addRoleToUser("musmus@orion","ROLE_MANAGER");
 
         };
+    }
+    @Bean
+    public OpenAPI customOpenApi(@Value("${application-description}") String description,
+                                 @Value("${application-version}") String version){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("SmartIQ Task Api")
+                        .version(version)
+                        .description(description)
+                        .license(new License().name("SmartIQ Licence")));
     }
 }
