348d98d97d5b7bad770e93df1026265224d35276
Yusuf Yildirim <yusufyildirim1025@gmail.com>
2022-08-06 12:30:49 +0300

    initialized Mail Producer module.

---
+++ Mail-Producer/src/test/java/io/smartiq/mailproducer/MailProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.smartiq.mailproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
